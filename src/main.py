793d6f2ec10d7526a1473e1d64fd35005067f95b
joshuamatos <joshua.matos@upr.edu>
2021-10-23 17:47:20 -0400

    Changing Utils location

--- src/main.py
+++ src/main.py
@@ -47,30 +47,6 @@ def reviews():
 @app.route('/reviews/<int:review_id>', methods=[GET, PUT, DELETE])
 def reviews_by_id(review_id):
     return 'Not implemented'
-
-from psycopg2 import connect
-
-'Status Codes'
-OK = 200
-CREATED = 201
-ACCEPTED = 202
-NOT_FOUND = 404
-BAD_REQUEST = 400
-
-DATABASE_CONFIG = {
-    'host' : 'localhost',
-    'user' : 'postgres',
-    'password' : 'inso',
-    'database' : 'Capstone_Inso'
-}
-
-def connect_db() -> connect:
-    try:
-        connection = connect(**DATABASE_CONFIG)
-    except Exception as e:
-        print(f'Connection to the database failed with error {e}')
-    return connection
-
-
+    
 if __name__ == "__main__":
     app.run()
---
+++ src/utils.py
@@ -0,0 +1,22 @@
+from psycopg2 import connect
+
+'Status Codes'
+OK = 200
+CREATED = 201
+ACCEPTED = 202
+NOT_FOUND = 404
+BAD_REQUEST = 400
+
+DATABASE_CONFIG = {
+    'host' : 'localhost',
+    'user' : 'postgres',
+    'password' : 'inso',
+    'database' : 'Capstone_Inso'
+}
+
+def connect_db() -> connect:
+    try:
+        connection = connect(**DATABASE_CONFIG)
+    except Exception as e:
+        print(f'Connection to the database failed with error {e}')
+    return connection
